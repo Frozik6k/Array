3c86d9d9cc4ffdd0717223b6de6f8e5018d56142
Frozik6k <mgc2000@bk.ru>
2024-11-25 01:07:37 +0500

    4 задачи Массивы 2 урок

--- src/Main.java
+++ src/Main.java
@@ -4,67 +4,49 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
+
+
         System.out.println("Задача №1");
-        int [] weight = new int [12];
-        int element = 1;
-        for (int i = 0; i < weight.length; i++){
-            weight[i] = element;
-            element++;
-            if (element >=4) element = 1;
+        float [] costs = { 34_543.23f, 12_446.50f, 3_700.00f, 60_300.00f, 23_000.00f };
+        float sum = 0;
+        for (float cost : costs) {
+            sum+=cost;
         }
-
-        float [] sand = {1.57f, 7.654f, 9.986f};
-
-        boolean [] indicators = {true, false, false, true, false, true, true};
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей.");
 
         System.out.println();
         System.out.println("Задача №2");
-        for (int i = 0; i < weight.length; i++){
-            System.out.print(weight[i]);
-            if (i < weight.length-1)
-                System.out.print(", ");
+        float [] costs1 = { 12_543.23f, 12_446.50f, 3_000.00f, 70_000.00f, 26_000.00f };
+        float min = costs1[0];
+        float max = costs1[0];
+        for (float cost : costs1){
+            if (min > cost) min = cost;
+            if (max < cost) max = cost;
         }
-        System.out.println();
-        for (int i = 0; i < sand.length; i++){
-            System.out.print(sand[i]);
-            if (i < sand.length-1)
-                System.out.print(", ");
-        }
-        System.out.println();
-        for (int i = 0; i < indicators.length; i++){
-            System.out.print(indicators[i]);
-            if (i < indicators.length-1)
-                System.out.print(", ");
-        }
-        System.out.println();
+        System.out.println("Минимальная сумма трат за неделю составила " + min + " рублей. " +
+                "Максимальная сумма трат за неделю составила " + max + " рублей.");
 
         System.out.println();
         System.out.println("Задача №3");
-        for (int i = weight.length-1; i >= 0; i--){
-            System.out.print(weight[i]);
-            if (i > 0)
-                System.out.print(", ");
+        float [] costs2 = { 10_543.23f, 1_446.50f, 6_800.00f, 8_000.00f, 75_000.00f };
+        float average = 0;
+        for (float cost : costs2){
+            average+=cost;
         }
-        System.out.println();
-        for (int i = sand.length-1; i >= 0; i--){
-            System.out.print(sand[i]);
-            if (i > 0)
-                System.out.print(", ");
-        }
-        System.out.println();
-        for (int i = indicators.length-1; i >= 0 ; i--){
-            System.out.print(indicators[i]);
-            if (i > 0)
-                System.out.print(", ");
-        }
-        System.out.println();
+        average/=costs2.length;
+        System.out.println("Средняя сумма трат за месяц составила " + average + " рублей.");
 
         System.out.println();
         System.out.println("Задача №4");
-        for (int i = 0; i < weight.length; i++){
-            if ( weight[i] % 2 == 1) weight[i]++;
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = 0; i < reverseFullName.length/2; i++) {
+            char temp = reverseFullName[i];
+            reverseFullName[i] = reverseFullName[reverseFullName.length - i - 1];
+            reverseFullName[reverseFullName.length - i - 1] = temp;
         }
-        System.out.println(Arrays.toString(weight));
+        for (char c : reverseFullName){
+            System.out.print(c);
+        }
 
 
 
